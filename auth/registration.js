8150f2a48ceba73796d4bfc7344a6b0024862d8e
Gabbygithub19 <161225777+Gabbygithub19@users.noreply.github.com>
2024-05-27 19:35:04 +0200

    second commit

---
+++ auth/registration.js
@@ -0,0 +1,32 @@
+const signUpForm = document.getElementById('signUpForm');
+
+signUpForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const formData = {
+    firstname: document.getElementById('firstname').value,
+    lastname: document.getElementById('lastname').value,
+    email: document.getElementById('email').value,
+    mobileNumber: document.getElementById('mobileNumber').value,
+    username: document.getElementById('username').value
+  };
+
+  try {
+    const response = await fetch('https://policy-link-rwanda-client-project-with.onrender.com/account/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    });
+
+    if (response.ok) {
+      console.log('User registered successfully');
+      window.location.href = '/LoginForm';
+    } else {
+      console.error('Failed to register user');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+});
